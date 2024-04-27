f82f0510bc7c3a71189d687eb65a42b96aefb0cc
carl <carl.skantz@helicon.ai>
2024-04-28 00:40:50 +0200

    Format bearer to look correct

--- bearer.sh
+++ bearer.sh
@@ -3,8 +3,8 @@
 # This script is used to generate a bearer token from the twitch API
 
 # Set the client ID and secret
-
 CLIENT_ID=""
 CLIENT_SECRET=""
+
 curl -X POST "https://id.twitch.tv/oauth2/token?client_id=$CLIENT_ID&client_secret=$CLIENT_SECRET&grant_type=client_credentials" >> secrets.py
 
